0fe91e8f3a5766964ddf106c64fba1ab565813bc
quentinmathieu <quentin.mth@gmail.com>
2023-01-20 12:10:18 +0100

    navBar+ css navBar

--- src/admin.php
+++ src/admin.php
@@ -3,8 +3,10 @@
 <head>
     <meta charset="utf-8">
     <title>Update</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="../css/admin.css">
+
 
 </head>
 
@@ -97,6 +99,13 @@
 
                 </div>
 
+            </div>
+            <div id="navContainer">
+                <nav class="navAdmin">
+                    <a href="../index.php" class="navBtn" style="padding:13px">Index</a>
+                    <a href="panier.php"class="navBtn fa-solid fa-basket-shopping" style="color:white; text-align:center;vertical-align: bottom; display:flex;align-items:center"></a>
+
+                </nav>
             </div>
         </div>
     </div>
