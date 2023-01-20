ea9311ee047fa331a9b4b9d8e236976bf169de84
quentinmathieu <quentin.mth@gmail.com>
2023-01-20 10:47:27 +0100

    fixed filters $dbPrice != null and add flash msg

--- src/admin.php
+++ src/admin.php
@@ -9,54 +9,19 @@
 </head>
 
 <body>
-    <div class="login-root">
+    <div>
 
-        <div class="box-root flex-flex flex-direction--column" style="min-height: 100vh;flex-grow: 1;">
-            <div class="loginbackground box-background--white padding-top--32" style="text-align : center">
-                <?php
-                session_start();
-                require_once("db-functions.php");
-                $pricings = findAll();
-                $message = (isset($_SESSION['message'])) ? $_SESSION['message'] : "";
-                echo $message;
+        <div style="min-height: 100vh;flex-grow: 1;">
+            <?php
+            session_start();
+            require_once("db-functions.php");
+            $pricings = findAll();
+            if (isset($_SESSION['message'])) {
+                echo $_SESSION['message'];
+                // unset($_SESSION['message']);
+            }
+            ?>
 
-                ?>
-                <div class="loginbackground-gridContainer">
-                    <div class="box-root flex-flex" style="grid-area: top / start / 8 / end;">
-                        <div class="box-root" style="background-image: linear-gradient(white 0%, rgb(247, 250, 252) 33%); flex-grow: 1;">
-                        </div>
-                    </div>
-                    <div class="box-root flex-flex" style="grid-area: 4 / 2 / auto / 5;">
-                        <div class="box-root box-divider--light-all-2 animationLeftRight tans3s" style="flex-grow: 1;">
-                        </div>
-                    </div>
-                    <div class="box-root flex-flex" style="grid-area: 6 / start / auto / 2;">
-                        <div class="box-root box-background--blue800" style="flex-grow: 1;"></div>
-                    </div>
-                    <div class="box-root flex-flex" style="grid-area: 7 / start / auto / 4;">
-                        <div class="box-root box-background--blue animationLeftRight" style="flex-grow: 1;"></div>
-                    </div>
-                    <div class="box-root flex-flex" style="grid-area: 8 / 4 / auto / 6;">
-                        <div class="box-root box-background--gray100 animationLeftRight tans3s" style="flex-grow: 1;">
-                        </div>
-                    </div>
-                    <div class="box-root flex-flex" style="grid-area: 2 / 15 / auto / end;">
-                        <div class="box-root box-background--cyan200 animationRightLeft tans4s" style="flex-grow: 1;">
-                        </div>
-                    </div>
-                    <div class="box-root flex-flex" style="grid-area: 3 / 14 / auto / end;">
-                        <div class="box-root box-background--blue animationRightLeft" style="flex-grow: 1;"></div>
-                    </div>
-                    <div class="box-root flex-flex" style="grid-area: 4 / 17 / auto / 20;">
-                        <div class="box-root box-background--gray100 animationRightLeft tans4s" style="flex-grow: 1;">
-                        </div>
-                    </div>
-                    <div class="box-root flex-flex" style="grid-area: 5 / 14 / auto / 17;">
-                        <div class="box-root box-divider--light-all-2 animationRightLeft tans3s" style="flex-grow: 1;">
-                        </div>
-                    </div>
-                </div>
-            </div>
             <div class="box-root padding-top--24 flex-flex flex-direction--column" style="flex-grow: 1; z-index: 9;">
                 <div class="box-root padding-top--48 padding-bottom--24 flex-flex flex-justifyContent--center">
                     <h1><a href="http://blog.stackfindover.com/" rel="dofollow">Update Pricing</a></h1>
@@ -78,7 +43,7 @@
                             </div>
                             <div class="field padding-bottom--24">
                                 <label for="Price">Price </label>
-                                <input type="number" id="Price" name="Price" value="<?= $pricing["Price"] ?>">
+                                <input type="number" step="0.01" id="Price" name="Price" value="<?= $pricing["Price"] ?>">
                             </div>
 
                         </div>
--- src/functions.php
+++ src/functions.php
@@ -11,7 +11,8 @@ case 'update':
     if(isset($_POST['submit'])){
         
         $dbName = filter_input(INPUT_POST, "Name", FILTER_SANITIZE_SPECIAL_CHARS);
-        $dbPrice = filter_input(INPUT_POST,"Price", FILTER_SANITIZE_NUMBER_FLOAT);
+        $price = str_replace(',', '.', $_POST['Price']);
+        $dbPrice = filter_var($price, FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
         $dbSale = filter_input(INPUT_POST, "Sale",  FILTER_SANITIZE_NUMBER_INT);
         $dbBandwidth = filter_input(INPUT_POST, "Bandwidth",  FILTER_SANITIZE_NUMBER_INT);
         $dbOnlinespace = filter_input(INPUT_POST, "Onlinespace",  FILTER_SANITIZE_NUMBER_INT);
@@ -19,15 +20,19 @@ case 'update':
         $dbDomain = filter_input(INPUT_POST, "Domain", FILTER_SANITIZE_SPECIAL_CHARS);
         $dbHiddenfees = filter_input(INPUT_POST, "HiddenFees",  FILTER_SANITIZE_SPECIAL_CHARS);
         
-        if ($dbName!=false  &&  $dbPrice !=false &&  $dbSale !=false &&  $dbBandwidth !=false &&  $dbOnlinespace !=false &&  $dbSupport !=false &&  $dbHiddenfees !=false &&  $dbDomain!=false){
+
+        if ($dbName  &&  ($dbPrice !==false && $dbPrice != null) &&  
+        ($dbSale !==false  && $dbSale!= null)&&  ($dbBandwidth !==false  && $dbBandwidth!= null)&&  
+        ($dbOnlinespace !==false  && $dbOnlinespace!= null)&&  ($dbSupport !==false  && $dbSupport!= null)&&  
+        ($dbHiddenfees !==false  && $dbHiddenfees!= null)&&  $dbDomain){
             UpdatePricing($dbName ,  $dbPrice ,  $dbSale ,  $dbBandwidth ,  $dbOnlinespace ,  $dbSupport ,  $dbHiddenfees ,  $dbDomain, $id);
-            $_SESSION['message'] = "<p>success</p>";
+            $_SESSION['message'] = "<p class='succes'>success</p>";
             header("Location:admin.php");
             die;
             
         }
         else{
-            $_SESSION['message'] = "<p>error</p>";
+            $_SESSION['message'] = "<p class='error'>ERROR</p>";
             header("Location:admin.php");
     
         }
